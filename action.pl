16a10fda540f1803a16ccd1e6407cc90b29de78b
Bryon <bryon@osuosl.org>
2014-01-24 12:54:20 -0800

    Fixed some typos, added bryon to requestor list, added return statement

--- action.pl
+++ action.pl
@@ -4,15 +4,18 @@ use List::MoreUtils 'any';
 @accounts = {
     'jnoah@osuosl.org',
     'jldugger@osuosl.org',
+    'bryon@osuosl.org',
 };
 
 my $PRIORITY = 10;
 
 # See if the requestor is in the list of high priority accounts
-my $requestor_addresses = $self->TicketObj->RequestorAdresses();
+my $requestor_addresses = $self->TicketObj->RequestorAddresses();
 my $in_list = any { /$requestor_addresses/ } @accounts;
 
 # If in the list, set the priority
 if ($in_list) {
-    $self->TicketObj->SetPriority($PRORITY);
+    $self->TicketObj->SetPriority( $PRIORITY );
 }
+
+return 1;
