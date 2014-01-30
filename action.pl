38f8b7e78cdeb77f5a1fdbe6d65a4bc0219db18b
Bryon <bryon@osuosl.org>
2014-01-30 08:59:50 -0800

    removing email addresses, adding some comments

--- action.pl
+++ action.pl
@@ -1,5 +1,6 @@
 ###############################
 #Custom Condition:
+#This will run the script only if the ticket was created in, or moved to, whatever queue this script is associated with
 if (($self->TransactionObj->Type eq "Create")||(($self->TransactionObj->Type eq "Set")&&($self->TransactionObj->Field eq "Queue"))) {
         return 1;
     }
@@ -14,30 +15,23 @@ return 1;  #this is important
 
 
 
-
-
 ###############################
 #Custom action cleanup code:
 use List::MoreUtils 'any';
 
-# high priority account list
-my @accounts = ('jnoah@osuosl.org','bryon@osuosl.org','jldugger@osuosl.org');
+# high priority account list, put VIP email addresses here
+my @accounts = ('');
 
 my $PRIORITY = 10;
 
 # See if the requestor is in the list of high priority accounts
 my @requestor_addresses = split(', ', $self->TicketObj->RequestorAddresses());
-
-
 my $in_list = 0;
 foreach my $val (@requestor_addresses) {
-#    $RT::Logger->info("-".$val."-");
     if ($val ~~ @accounts){
         $in_list = 1;
     }
-#    $RT::Logger->info("-".$in_list."-");
   }
-
 if ($in_list) {
    $self->TicketObj->SetPriority( $PRIORITY );
 }
